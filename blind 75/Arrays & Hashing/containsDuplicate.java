d708b78e430c737299465145c273160e99c15b68
Nielsen <nielsenangrio@gmail.com>
2022-05-06 12:57:59 -0700

    Initialize

---
+++ blind 75/Arrays & Hashing/containsDuplicate.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+public class containsDuplicate {
+    public static boolean containDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            } else {
+                set.add(num);
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 1 };
+
+        System.out.println(containDuplicate(arr));
+    }
+}
