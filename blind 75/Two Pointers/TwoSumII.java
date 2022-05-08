4c50273204891a3238bd66fd2aed943ee0e3be4e
Nielsen <nielsenangrio@gmail.com>
2022-05-08 15:10:52 -0700

    Optimization

--- blind 75/Two Pointers/TwoSumII.java
+++ blind 75/Two Pointers/TwoSumII.java
@@ -1,24 +1,19 @@
-import java.util.HashMap;
-
 public class TwoSumII {
     public static int[] twoSum(int[] numbers, int target) {
-        int[] ret = new int[2];
-        HashMap<Integer, Integer> mem = new HashMap<Integer, Integer>(); // Key is number looked for, value is the index
+        int start = 0;
+        int end = numbers.length - 1;
 
-        for (int i = 0; i < numbers.length; i++) {
-            int num1 = numbers[i];
-            int num2 = target - num1;
+        while (start < end) {
+            if (numbers[start] + numbers[end] == target)
+                break;
 
-            if (mem.containsKey(num2)) {
-                ret[0] = mem.get(num2) + 1;
-                ret[1] = i + 1;
-                return ret;
-            }
-
-            mem.put(num1, i);
+            if (numbers[start] + numbers[end] < target)
+                start++;
+            else
+                end--;
         }
 
-        return ret;
+        return new int[] { start + 1, end + 1 };
     }
 
     public static void main(String[] args) {
